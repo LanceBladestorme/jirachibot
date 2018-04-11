6b809d4856d21ae06a74a14fa6dee58cf945c14b
LanceBladestorme <lancebladestorme@gmail.com>
2018-04-11 19:03:44 -0400

    Update guildMemberAdd.js

--- devents/guildMemberAdd.js
+++ devents/guildMemberAdd.js
@@ -1,5 +1,4 @@
-exports.run = (client, message) => {
-var role = message.guild.roles.find('name', 'Trebled Soul')
+exports.run = (client, member) => {
+var role = member.guild.roles.find('name', 'Trebled Soul')
 member.addRole('Trebled Soul');
-  if (!message.content.startsWith(config.prefix)) return;
 };
