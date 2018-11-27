efb57ea47cd664b0a57e3920160ad3f05a083d22
LanceBladestorme <lancebladestorme@gmail.com>
2018-11-26 21:13:21 -0500

    Update guildMemberAdd.js

--- devents/guildMemberAdd.js
+++ devents/guildMemberAdd.js
@@ -1,4 +1,6 @@
 exports.run = (client, member) => {
 var role = member.guild.roles.find('name', 'Trebled Soul')
+var role2 = member.guild.roles.find('name', 'Pie Legion Villager')
 member.addRole(role);
+member.addRole(role2);
 };
